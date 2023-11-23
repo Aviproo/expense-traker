0ecd4d8489e1bc5d9267b628364f0974560582fd
AVINASH KUMAR <Aviproo>
2023-11-23 15:04:51 +0530

    Verify User Email

--- src/Components/Pages/Welcome.js
+++ src/Components/Pages/Welcome.js
@@ -1,14 +1,41 @@
-import { useNavigate } from "react-router-dom";
+import { json, useNavigate } from "react-router-dom";
 import classes from "./Welcome.module.css";
 const Welcome = () => {
   const navigate = useNavigate();
+
+  const verify = () => {
+    const url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyCvzryAZ4dVWRP2PJ-eM4EE78m3NrDF5F0";
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        requestType: "VERIFY_EMAIL",
+        idToken: localStorage.getItem("token"),
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      if (res.ok) {
+        return res.json().then((data) => {
+          alert("We have sent a varification link on your email");
+        });
+      } else {
+        return res.json().then((data) => {
+          alert(data.error.message);
+        });
+      }
+    });
+  };
   return (
     <div>
       <div className={classes.header}>
         <div>Welcome to Expense Tracker!!!</div>
+
         <div className={classes.incopleteProfile}>
-          Your Profile is Incomplete.{" "}
+          Your Profile is Incomplete.
           <span onClick={() => navigate("profile")}>Complete now</span>
+          <button onClick={verify}>Verify email</button>
         </div>
       </div>
       <hr />
